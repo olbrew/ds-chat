867575ae6e5852f2986e0c2b1ec9a51cb4a98b01
eduard <fc.puntje@gmail.com>
2015-10-29 10:27:03 +0100

    Hello World Avro working

---
+++ src/avro/hello/client/HelloClient.java
@@ -0,0 +1,27 @@
+package avro.hello.client;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import org.apache.avro.ipc.SaslSocketTransceiver;
+import org.apache.avro.ipc.Transceiver;
+import org.apache.avro.ipc.specific.SpecificRequestor;
+import avro.hello.proto.Hello;
+
+public class HelloClient {
+	public static void main(String[] args) {
+		try {
+			Transceiver client = new SaslSocketTransceiver(new InetSocketAddress(10000));
+			
+			Hello proxy = (Hello) SpecificRequestor.getClient(Hello.class, client);
+			
+			CharSequence response = proxy.sayHello("Bob");
+			System.out.println(response);
+			
+			client.close();
+		} catch(IOException e) {
+			System.err.println("Error connecting to server ...");
+			e.printStackTrace(System.err);
+			System.exit(1);
+		}
+	}
+}
