d2345bc689926ef438ef4856893c372f0dddf416
puntje <fc.puntje@gmail.com>
2016-01-07 17:07:37 +0100

    Keeps the client running after closing the video streamer.

--- src/xuggler/VideoImage.java
+++ src/xuggler/VideoImage.java
@@ -22,105 +22,100 @@ package xuggler;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Image;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.awt.image.BufferedImage;
 
 import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
 
+import org.apache.avro.AvroRemoteException;
+
+import avro.chat.proto.ChatClientServer;
+
 /**
- * This class just displays a 2d graphic on a Swing window.  It's
- * only here so the video playback demos look simpler.  Please don't
- * reuse this component; why?  Because I know next to nothing
- * about Swing, and this is probably busted.
+ * This class just displays a 2d graphic on a Swing window. It's only here so
+ * the video playback demos look simpler. Please don't reuse this component;
+ * why? Because I know next to nothing about Swing, and this is probably busted.
  * <p>
  * Of note though, is this class has NO XUGGLER dependencies.
  * </p>
+ * 
  * @author aclarke
  *
  */
 @SuppressWarnings("serial")
-public class VideoImage extends JFrame
-{
+public class VideoImage extends JFrame {
+    private final ImageComponent mOnscreenPicture;
+    private ChatClientServer privateProxy;
 
-  /**
-   * To avoid a warning... 
-   */
+    public VideoImage(ChatClientServer proxy) {
+        super("Video stream.");
+        privateProxy = proxy;
+        mOnscreenPicture = new ImageComponent();
+        this.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+        getContentPane().add(mOnscreenPicture);
+        this.setVisible(true);
+        this.pack();
 
-  private final ImageComponent mOnscreenPicture;
-
-
-  /**
-   * Create the frame
-   */
-
-  public VideoImage()
-  {
-    super();
-    mOnscreenPicture = new ImageComponent();
-    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    getContentPane().add(mOnscreenPicture);
-    this.setVisible(true);
-    this.pack();
-  }
-  
-  public void setImage(final BufferedImage aImage)
-  {
-    mOnscreenPicture.setImage(aImage);
-  }
-
-public class ImageComponent extends JComponent
-  {
-    /**
-     * yeah... good idea to add this.
-     */
-    private Image mImage;
-    private Dimension mSize;
-
-    public void setImage(Image image)
-    {
-      SwingUtilities.invokeLater(new ImageRunnable(image));
+        this.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+                setVisible(false);
+                dispose();
+                
+                try {
+                    privateProxy.stopVideoStream();
+                } catch (AvroRemoteException e1) {
+                    System.err.println("VideoImage bad private proxy");
+                    e1.printStackTrace();
+                }
+            }
+        });
     }
-    
-    public void setImageSize(Dimension newSize)
-    {
-    }
-    
-    private class ImageRunnable implements Runnable
-    {
-      private final Image newImage;
 
-      public ImageRunnable(Image newImage)
-      {
-        super();
-        this.newImage = newImage;
-      }
-  
-      public void run()
-      {
-        ImageComponent.this.mImage = newImage;
-        final Dimension newSize = new Dimension(mImage.getWidth(null), 
-          mImage.getHeight(null));
-        if (!newSize.equals(mSize))
-        {
-          ImageComponent.this.mSize = newSize;
-          VideoImage.this.setSize(mImage.getWidth(null), mImage.getHeight(null));
-          VideoImage.this.setVisible(true);
+    public void setImage(final BufferedImage image) {
+        mOnscreenPicture.setImage(image);
+    }
+
+    public class ImageComponent extends JComponent {
+        private Image mImage;
+        private Dimension mSize;
+
+        public void setImage(Image image) {
+            SwingUtilities.invokeLater(new ImageRunnable(image));
         }
-        repaint();
-      }
-    }
-    
-    public ImageComponent()
-    {
-      mSize = new Dimension(0, 0);
-      setSize(mSize);
-    }
 
-    public synchronized void paint(Graphics g)
-    {
-      if (mImage != null)
-        g.drawImage(mImage, 0, 0, this);
+        private class ImageRunnable implements Runnable {
+            private final Image newImage;
+
+            public ImageRunnable(Image newImage) {
+                super();
+                this.newImage = newImage;
+            }
+
+            public void run() {
+                ImageComponent.this.mImage = newImage;
+                final Dimension newSize = new Dimension(mImage.getWidth(null), mImage.getHeight(null));
+                if (!newSize.equals(mSize)) {
+                    ImageComponent.this.mSize = newSize;
+                    VideoImage.this.setSize(mImage.getWidth(null), mImage.getHeight(null));
+                    VideoImage.this.setVisible(true);
+                }
+                repaint();
+            }
+        }
+
+        public ImageComponent() {
+            mSize = new Dimension(0, 0);
+            setSize(mSize);
+        }
+
+        public synchronized void paint(Graphics g) {
+            if (mImage != null)
+                g.drawImage(mImage, 0, 0, this);
+        }
     }
-  }
 }
